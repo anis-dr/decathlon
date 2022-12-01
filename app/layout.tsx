bc7c5085638ceef926193d15bd1607539f0e71a3
Anis Lazaar <anis00723@gmail.com>
2022-12-01 22:22:31 +0100

    :sparkles: init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import React from 'react'
 
 export default function RootLayout({
   children,
@@ -11,7 +12,9 @@ export default function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
+      <head>
+        <title>Create Next App</title>
+      </head>
       <body>{children}</body>
     </html>
   )
