5a807581b4a6311dfe335e927dbe6de483adb51f
Anis Lazaar <anis00723@gmail.com>
2022-12-02 18:11:38 +0100

    :pencil2: fix typo

--- components/OurTeam.tsx
+++ components/OurTeam.tsx
@@ -2,24 +2,28 @@ const people = [
   {
     name: 'Bassem Dimassi',
     role: 'JavaScript Developer',
+    email: 'bassemdimassi99@gmail.com',
     imageUrl:
       'https://scontent.ftun9-1.fna.fbcdn.net/v/t39.30808-1/238810472_2048655508626393_3412035247471087875_n.jpg?stp=dst-jpg_p480x480&_nc_cat=106&ccb=1-7&_nc_sid=7206a8&_nc_ohc=Oupd8Fjb4K8AX9l5mpW&_nc_ht=scontent.ftun9-1.fna&oh=00_AfDwP0mfSFW-lUp-wux6_LHZB93Pjn4ckKt3KpjmdYl8wA&oe=638DC222',
   },
   {
     name: 'Anis Lazaar',
     role: 'Full Stack Developer',
+    email: 'anis00723@gmail.com',
     imageUrl:
       'https://scontent.ftun9-1.fna.fbcdn.net/v/t39.30808-1/317086044_5877436412299987_1129762482173363525_n.jpg?stp=c0.0.480.480a_dst-jpg_p480x480&_nc_cat=108&ccb=1-7&_nc_sid=7206a8&_nc_ohc=0Xw5Y3iTZL0AX-V1dvL&_nc_ht=scontent.ftun9-1.fna&oh=00_AfDYPHHMFf7B5Xjw3btYKnR14kODLTFtLaINZtprxWPDnw&oe=638EE869',
   },
   {
     name: 'Abdelaziz Ben Salem',
-    role: 'Full Developer',
+    role: 'Full Stack Developer',
+    email: 'bensalemabdelaziz97@gmail.com',
     imageUrl:
       'https://scontent.ftun9-1.fna.fbcdn.net/v/t39.30808-1/279256668_7500940736612840_6402174375253931671_n.jpg?stp=dst-jpg_p480x480&_nc_cat=107&ccb=1-7&_nc_sid=7206a8&_nc_ohc=GbI67Lqg2akAX-ICycq&_nc_ht=scontent.ftun9-1.fna&oh=00_AfCAqnMEA6N-fOnxV7t4v3HWnIx_URKdytSJ2R4rbGs41g&oe=638DE42F',
   },
   {
     name: 'Walid Mabrouk',
     role: 'Full Stack Developer',
+    email: 'walidmabr123@gmail.com',
     imageUrl:
       'https://scontent.ftun9-1.fna.fbcdn.net/v/t1.6435-1/96518437_2922725547844066_8320260450251964416_n.jpg?stp=dst-jpg_p480x480&_nc_cat=103&ccb=1-7&_nc_sid=7206a8&_nc_ohc=DT1vhl_Pf88AX8gVcTY&_nc_ht=scontent.ftun9-1.fna&oh=00_AfAaqA3XbAivGmTqKqyZOFLhb0FiGcJrUzxjvbCwUq9LpA&oe=63B0CE73',
   },
@@ -54,6 +58,9 @@ const OurTeam = () => {
                     <div className="space-y-1 text-lg font-medium leading-6">
                       <h3>{person.name}</h3>
                       <p className="text-indigo-600">{person.role}</p>
+                      <div className="mt-2 text-base text-gray-500">
+                        <p>{person.email}</p>
+                      </div>
                     </div>
                   </div>
                 </li>
